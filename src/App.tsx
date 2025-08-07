180c6b8c17f54b33270c679d024d65c852eeb6f5
ajaynrg <ajaynramangoudar@gmail.com>
2025-08-07 21:56:28 +0530

    Add Radix UI button component and update dependencies; refactor App component to use new button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import { Button } from './components/ui/button';
+
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="cursor-pointer">Click me</Button>
+    </div>
   )
 }
 
