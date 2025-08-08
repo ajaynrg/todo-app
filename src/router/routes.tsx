b8d9d2e09e9c3babc4db657e28d64d28e2b34f50
ajaynrg <ajaynramangoudar@gmail.com>
2025-08-08 21:41:40 +0530

    Using React Router Data Mode
    Update package dependencies to include react-router v7.8.0; refactor main entry point to use RouterProvider for routing management.

---
+++ src/router/routes.tsx
@@ -0,0 +1,11 @@
+import App from "@/App";
+import {
+    createBrowserRouter,
+} from "react-router";
+
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    },
+]);
