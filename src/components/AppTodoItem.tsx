8b9c207f0625f04a6f907cebe13c5f283379d120
ajaynrg <ajaynramangoudar@gmail.com>
2025-08-09 16:05:15 +0530

    Disable checkbox for completed tasks in AppTodoItem to prevent further interaction; enhance user experience by clearly indicating task status.

--- src/components/AppTodoItem.tsx
+++ src/components/AppTodoItem.tsx
@@ -1,29 +1,22 @@
 import type { Todo } from "@/store/useTodoStore";
 import useTodoStore from "@/store/useTodoStore";
-import { useState } from "react";
 import { Checkbox } from "./ui/checkbox";
 import { TableCell, TableRow } from "./ui/table";
 
 export function AppTodoItem({todoItem}: {todoItem:Todo}){
     const toggleTodo = useTodoStore((state) => state.toggleTodo)
-    const [animation, setAnimation] = useState(false);
-
-    const taskCompleted = (id: number) =>{
-        setAnimation(true)
-        setTimeout(()=>{
-            toggleTodo(id)
-        }, 1000)
-    }
     return (
-        <TableRow className={animation ? '':''}>
+        <TableRow>
             <TableCell>{todoItem.id}</TableCell>
             <TableCell>{todoItem.priority}</TableCell>
             <TableCell>{todoItem.text}</TableCell>
             <TableCell>{todoItem.endDate.toDateString()}</TableCell>
             <TableCell>
                 <Checkbox 
+                    className="cursor-pointer"
                     checked={todoItem.completed}
-                    onCheckedChange={() => {taskCompleted(todoItem.id); todoItem.completed = true;}}
+                    onCheckedChange={() => {toggleTodo(todoItem.id);}}
+                    disabled={todoItem.completed}
                 />
             </TableCell>
         </TableRow>
