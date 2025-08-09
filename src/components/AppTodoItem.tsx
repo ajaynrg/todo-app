0b0cf27601addbc0fcd90e7c9ca1f09dbd4d6599
ajaynrg <ajaynramangoudar@gmail.com>
2025-08-09 15:52:52 +0530

    Implement animation for task completion in AppTodoItem; add state management to trigger animation before toggling todo status.

--- src/components/AppTodoItem.tsx
+++ src/components/AppTodoItem.tsx
@@ -1,12 +1,21 @@
 import type { Todo } from "@/store/useTodoStore";
 import useTodoStore from "@/store/useTodoStore";
+import { useState } from "react";
 import { Checkbox } from "./ui/checkbox";
 import { TableCell, TableRow } from "./ui/table";
 
 export function AppTodoItem({todoItem}: {todoItem:Todo}){
     const toggleTodo = useTodoStore((state) => state.toggleTodo)
+    const [animation, setAnimation] = useState(false);
+
+    const taskCompleted = (id: number) =>{
+        setAnimation(true)
+        setTimeout(()=>{
+            toggleTodo(id)
+        }, 1000)
+    }
     return (
-        <TableRow>
+        <TableRow className={animation ? '':''}>
             <TableCell>{todoItem.id}</TableCell>
             <TableCell>{todoItem.priority}</TableCell>
             <TableCell>{todoItem.text}</TableCell>
@@ -14,8 +23,7 @@ export function AppTodoItem({todoItem}: {todoItem:Todo}){
             <TableCell>
                 <Checkbox 
                     checked={todoItem.completed}
-                    onCheckedChange={() => toggleTodo(todoItem.id)}
-                    disabled={todoItem.completed}
+                    onCheckedChange={() => {taskCompleted(todoItem.id); todoItem.completed = true;}}
                 />
             </TableCell>
         </TableRow>
